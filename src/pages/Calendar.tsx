54abd314cfcccbe07a8588720db855da107ac4dd
Vailantan <vailantan@3003.com>
2023-10-13 13:23:10 +0530

    Dashboard changed

--- src/pages/Calendar.tsx
+++ src/pages/Calendar.tsx
@@ -53,10 +53,10 @@ const Calendar = () => {
                   </span>
                   <div className="event invisible absolute left-2 z-99 mb-1 flex w-[200%] flex-col rounded-sm border-l-[3px] border-primary bg-gray px-3 py-1 text-left opacity-0 group-hover:visible group-hover:opacity-100 dark:bg-meta-4 md:visible md:w-[190%] md:opacity-100">
                     <span className="event-name text-sm font-semibold text-black dark:text-white">
-                      Redesign Website
+                     Dahisar 
                     </span>
                     <span className="time text-sm font-medium text-black dark:text-white">
-                      1 Dec - 2 Dec
+                      3.5KG
                     </span>
                   </div>
                 </div>
@@ -198,10 +198,10 @@ const Calendar = () => {
                   </span>
                   <div className="event invisible absolute left-2 z-99 mb-1 flex w-[300%] flex-col rounded-sm border-l-[3px] border-primary bg-gray px-3 py-1 text-left opacity-0 group-hover:visible group-hover:opacity-100 dark:bg-meta-4 md:visible md:w-[290%] md:opacity-100">
                     <span className="event-name text-sm font-semibold text-black dark:text-white">
-                      App Design
+                      Borivali
                     </span>
                     <span className="time text-sm font-medium text-black dark:text-white">
-                      25 Dec - 27 Dec
+                    13KG
                     </span>
                   </div>
                 </div>
